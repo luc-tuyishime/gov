c71e7c0a928ba69d6ed65e6bccf5f46939e3048c
luc <luctunechi45@gmail.com>
2018-10-22 18:53:20 +0200

    Updates

---
+++ precache-manifest.16d20c9a7e673001a01f9bb7838447cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/luc-tuyishime/gov/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e06f1ecf0e05cc729c3",
+    "url": "/luc-tuyishime/gov/static/js/runtime~main.2e06f1ec.js"
+  },
+  {
+    "revision": "5a17ad92bd8117e754fc",
+    "url": "/luc-tuyishime/gov/static/js/main.5a17ad92.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/luc-tuyishime/gov/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "5a17ad92bd8117e754fc",
+    "url": "/luc-tuyishime/gov/static/css/main.79d70cad.chunk.css"
+  },
+  {
+    "revision": "050c7abcfc0202d4ef175ed87804aa68",
+    "url": "/luc-tuyishime/gov/index.html"
+  }
+];
