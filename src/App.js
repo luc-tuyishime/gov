049010f1cfe848477ac88f97adc2798ebfa681c7
luc <luctunechi45@gmail.com>
2018-10-30 18:26:38 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import Ministry from "./pages/Ministry"
 import MinalocWebsite from "./pages/MinalocWebsite"
@@ -9,9 +9,9 @@ import "./App.css"
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/gov/">
         <div>
-          <Route exact basename="/gov/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route path="/minaloc" component={Ministry} />
           <Route path="/minaloc-website" component={MinalocWebsite} />
           <Route path="/local-gov" component={LocalGov} />
